2c17be9ef04fe488095df55a7c153f1977eb6584
Anton Stasenko <jahdie@yandex.ru>
2023-02-04 16:44:26 +0700

    add feedback

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -76,3 +77,14 @@ class CourseTeachers(BaseModelAbstract):
     class Meta:
         verbose_name = _("Teacher")
         verbose_name_plural = _("Teachers")
+
+
+class CourseFeedback(BaseModelAbstract):
+    RATING = ((5, "⭐⭐⭐⭐⭐"), (4, "⭐⭐⭐⭐"), (3, "⭐⭐⭐"), (2, "⭐⭐"), (1, "⭐"))
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, verbose_name=_("Course"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("User"))
+    feedback = models.TextField(default=_("No feedback"), verbose_name=_("Feedback"))
+    rating = models.SmallIntegerField(choices=RATING, default=5, verbose_name=_("Rating"))
+
+    def __str__(self):
+        return f"{self.course} ({self.user})"
